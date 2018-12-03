7030cf20e1a1541344c86dcf5f10c7920f8c7f6a
silviomedice <silvio.medice@gmail.com>
2018-12-03 15:27:16 +0000

    Update index.js

--- index.js
+++ index.js
@@ -40,17 +40,38 @@ export default class RestClient {
       Object.assign(opts, { body: JSON.stringify(body) });
     }
     const fetchPromise = () => fetch(fullRoute, opts);
-    const extractResponse = response =>
-      response.text().then(text => text? JSON.parse(text) : undefined);
-
     if (this.devMode && this.simulatedDelay > 0) {
       // Simulate an n-second delay in every request
       return this._simulateDelay()
         .then(() => fetchPromise())
-        .then(extractResponse);
+          .then(response => 
+            { 
+              if(response.ok){
+                return response.json();
+              } else {
+                const statusCode = response.status;
+                const data = response.json();
+  
+                return Promise.all([statusCode, data]).then(res => ({
+                  status: res[0], ...res[1]
+                }));
+              }
+            });
     } else {
-      return fetchPromise()
-        .then(extractResponse);
+      return fetchPromise()        
+        .then(response => 
+          { 
+            if(response.ok){
+              return response.json();
+            } else {
+              const statusCode = response.status;
+              const data = response.json();
+
+              return Promise.all([statusCode, data]).then(res => ({
+                status: res[0], ...res[1]
+              }));
+            }
+          });
     }
   }
 
